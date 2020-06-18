fd45433c76a836a8ad5c11ba6bbd0922784c1f6f
techpress.rocks <ibmeister@gmail.com>
2020-06-18 19:43:22 +0200

    First Commit

---
+++ class-woocommerce-simple-twint-gateway.php
@@ -0,0 +1,158 @@
+<?php 
+
+class WC_Simple_Twint_Gateway extends WC_Payment_Gateway {
+
+    private $order_status;
+
+	public function __construct(){
+		$this->id = 'simple_twint';
+		$this->icon = plugin_dir_url( __FILE__ ) . 'img/twint_acceptance_mark_pos_100px.png';
+		$this->twintinfo = plugin_dir_url( __FILE__ ) . 'img/simple-twint-twint-info.jpg';
+		$this->method_title = __('Bezahlen Sie bequem mit Twint','woocommerce-simple-twint-gateway');
+		$this->method_description = __( 'Have your customers pay with the Twint app', 'woocommerce-simple-twint-gateway' );
+		$this->title = $this->get_option('title');
+		$this->has_fields = true;
+		$this->init_form_fields();
+		$this->init_settings();
+		$this->enabled = $this->get_option('enabled');
+		$this->description = $this->get_option('description');
+		$this->qr_code_img = $this->get_option('qr_code_img');
+		$this->order_status = $this->get_option('order_status');
+		$this->hide_text_box = $this->get_option('hide_text_box');
+
+		add_action( 'woocommerce_update_options_payment_gateways_'.$this->id, array($this, 'process_admin_options' ) );
+		add_action( 'woocommerce_thankyou', array( $this, 'simple_twint_thankyou_page' ) );
+	}
+
+	public function init_form_fields(){
+		$this->form_fields = array(
+			'enabled' => array(
+			'title' 		=> __( 'Enable/Disable', 'woocommerce-simple-twint-gateway' ),
+			'type' 			=> 'checkbox',
+			'label' 		=> __( 'Enable Simple twint Payment Gateway', 'woocommerce-simple-twint-gateway' ),
+			'default' 		=> 'yes'
+			),
+			'title' => array(
+				'title' 		=> __( 'Method Title', 'woocommerce-simple-twint-gateway' ),
+				'type' 			=> 'text',
+				'description' 	=> __( 'This controls the title', 'woocommerce-simple-twint-gateway' ),
+				'default'		=> __( 'Bequeme Ratenzahlung in Monatsraten', 'woocommerce-simple-twint-gateway' ),
+				'desc_tip'		=> true,
+			),
+			'description' => array(
+				'title' => __( 'Customer Message', 'woocommerce-simple-twint-gateway' ),
+				'type' => 'textarea',
+				'css' => 'width:500px;',
+				'default' => __( 'Sie können den Bestellbetrag bequem mit Ihrer Twint-App begleichen.', 'woocommerce-simple-twint-gateway' ),
+				'description' 	=> __( 'The general message which you want it to appear to the customer in the checkout page.', 'woocommerce-simple-twint-gateway' ),
+			),
+			'qr_code_img' => array(
+				'title' 		=> __( 'Link to QR-Code image (copy the link to the image from the media libary and paste here)', 'woocommerce-simple-twint-gateway' ),
+				'type' 			=> 'text',
+				'description' 	=> __( 'Link to QR-Code image (copy the link to the image from the media libary and paste here', 'woocommerce-simple-twint-gateway' ),
+				'default'		=> __( 'Link to QR-Code image (copy the link to the image from the media libary and paste here', 'woocommerce-simple-twint-gateway' ),
+				'desc_tip'		=> true,
+			),			
+			'order_status' => array(
+				'title' => __( 'Order Status After The Checkout', 'woocommerce-simple-twint-gateway' ),
+				'type' => 'select',
+				'options' => wc_get_order_statuses(),
+				'default' => 'wc-on-hold',
+				'description' 	=> __( 'The default order status if this gateway used in payment.', 'woocommerce-simple-twint-gateway' ),
+			),
+			'hide_text_box' => array(
+				'title' 		=> __( 'Hide Admin Notice', 'woocommerce-simple-twint-gateway' ),
+				'type' 			=> 'checkbox',
+				'label' 		=> __( 'Hide', 'woocommerce-simple-twint-gateway' ),
+				'default' 		=> 'no',
+				'description' 	=> __( 'If you do not want to show the admin notice box to customers (so they can send you a message), enable this option.', 'woocommerce-simple-twint-gateway' ),
+			),					
+		);
+	}
+	/**
+	 * Admin Panel Options
+	 * - Options for bits like 'title' and availability on a country-by-country basis
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function admin_options() {
+		?>
+		<h3><?php _e( 'Simple Twint Gateway', 'woocommerce-simple-twint-gateway' ); ?></h3>
+			<div id="poststuff">
+				<div id="post-body" class="metabox-holder columns-1">
+					<div id="post-body-content">
+						<table class="form-table">
+							<?php $this->generate_settings_html();?>
+						</table><!--/.form-table-->
+					</div>
+				</div>
+			</div>	
+				<?php
+	}
+
+	public function validate_fields() {
+	    if($this->text_box_required === 'no'){
+	        return true;
+        }
+
+	    if($this->hide_text_box === 'no'){
+			$textbox_value = (isset($_POST['other_payment-admin-note']))? trim($_POST['other_payment-admin-note']): '';
+			if($textbox_value === ''){
+				wc_add_notice( __('Please, complete the payment information.','woocommerce-simple-twint-gateway'), 'error');
+				return false;
+			}
+			return true;
+		}	
+	}
+
+	public function process_payment( $order_id ) {
+		global $woocommerce;
+		$order = new WC_Order( $order_id );
+		// Mark as on-hold (we're awaiting the cheque)
+		$order->update_status($this->order_status, __( 'Awaiting payment', 'woocommerce-simple-twint-gateway' ));
+		// Reduce stock levels
+		wc_reduce_stock_levels( $order_id );
+		if(isset($_POST[ $this->id.'-admin-note']) && trim($_POST[ $this->id.'-admin-note'])!=''){
+			$order->add_order_note(esc_html($_POST[ $this->id.'-admin-note']),1);
+		}
+		// Remove cart
+		$woocommerce->cart->empty_cart();
+		// Return thankyou redirect
+		return array(
+			'result' => 'success',
+			'redirect' => $this->get_return_url( $order )
+		);	
+	}
+
+	public function payment_fields(){
+	    ?>
+		<fieldset>
+			<p class="form-row form-row-wide">
+                <label for="<?php echo $this->id; ?>-admin-note"><?php echo ($this->description); ?> </label>
+                <?php if($this->hide_text_box !== 'yes'){ ?>
+				    <textarea id="<?php echo $this->id; ?>-admin-note" class="input-text" type="text" name="<?php echo $this->id; ?>-admin-note"></textarea>
+                <?php } ?>
+			</p>						
+			<div class="clear"></div>
+		</fieldset>
+		<?php
+	}
+
+	/**
+	* Output for the order received page.
+	*/
+	public function simple_twint_thankyou_page( $order_id ) {
+		$order = wc_get_order( $order_id );
+		if ( $order->get_payment_method() === 'simple_twint' ) {
+			$simple_twint_currency = $order->get_currency();
+			$simple_twint_ordertotal = $order->get_total();
+			echo '<img class="checkout-twint-img" style="display: block;margin: 0 auto;" src="'. $this->twintinfo . '">';
+			echo '<h3>' . __('Bitte überweisen Sie den folgenden Betrag:', 'woocommerce-simple-twint-gateway') . '</h3>';
+			echo '<p><b>' . $simple_twint_currency. ' ' . $simple_twint_ordertotal . '</b></p>';
+			if ( $this->qr_code_img ) {
+				echo '<img src="'. $this->qr_code_img . '">';
+			}	
+		}
+	}	
+}
---
+++ woocommerce-simple-twint-gateway.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name:       		WooCommerce Simple Twint Gateway
+ * Plugin URI:        		https://1daywebsite.ch
+ * Description:       		A simple way to add Twint to the checkout
+ * Version:           		1.0.0
+ * Author:            		AFB
+ * Author URI:        		https://1daywebsite.ch
+ * Tested up to:			5.4.1 
+ * WC requires at least:	3.0
+ * WC tested up to:			4.2
+ * Text Domain:       		woocommerce-simple-twint-gateway
+ * Domain Path: 			/languages
+ * License:           		GPL-2.0+
+ * License URI:       		http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+$active_plugins = apply_filters('active_plugins', get_option('active_plugins'));
+
+if( simple_twint_gateway_is_woocommerce_active() ){
+	/**
+	* Add new payment gateway class: WC_Simple_Twint_Gateway
+	*/
+	add_filter('woocommerce_payment_gateways', 'add_simple_twint_gateway');
+	function add_simple_twint_gateway( $gateways ){
+		$gateways[] = 'WC_Simple_Twint_Gateway';
+		return $gateways; 
+	}
+	/**
+	* Load class file and load plugin text domain
+	*/
+	add_action('plugins_loaded', 'init_simple_twint_gateway');
+	function init_simple_twint_gateway(){
+		require 'class-woocommerce-simple-twint-gateway.php';
+	}
+	add_action( 'plugins_loaded', 'simple_twint_gateway_load_plugin_textdomain' );
+	function simple_twint_gateway_load_plugin_textdomain() {
+	  load_plugin_textdomain( 'woocommerce-simple-twint-gateway', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	}
+}
+/**
+* Check if WooCommerce is active, otherwise don't run plugin
+* @return bool
+*/
+function simple_twint_gateway_is_woocommerce_active() {
+	$active_plugins = (array) get_option('active_plugins', array());
+	if (is_multisite()) {
+		$active_plugins = array_merge($active_plugins, get_site_option('active_sitewide_plugins', array()));
+	}
+	return in_array('woocommerce/woocommerce.php', $active_plugins) || array_key_exists('woocommerce/woocommerce.php', $active_plugins);
+}
